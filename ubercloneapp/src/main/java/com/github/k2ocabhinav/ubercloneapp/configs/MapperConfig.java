39b293b821a8a6fc5e22272a04fc36a3f3cc8083
Abhinav Prasad <k2ocabhinav22@gmail.com>
2024-07-27 19:18:09 +0530

    chores: Implement model mapper object with custom converter for Point obj

--- ubercloneapp/src/main/java/com/github/k2ocabhinav/ubercloneapp/configs/MapperConfig.java
+++ ubercloneapp/src/main/java/com/github/k2ocabhinav/ubercloneapp/configs/MapperConfig.java
@@ -1,5 +1,8 @@
 package com.github.k2ocabhinav.ubercloneapp.configs;
 
+import com.github.k2ocabhinav.ubercloneapp.dto.PointDto;
+import com.github.k2ocabhinav.ubercloneapp.utils.GeometryUtil;
+import org.locationtech.jts.geom.Point;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +11,23 @@ import org.springframework.context.annotation.Configuration;
 public class MapperConfig {
 
     @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.typeMap(PointDto.class, Point.class).setConverter(converter -> {
+            PointDto pointDto = converter.getSource();
+            return GeometryUtil.createPoint(pointDto);
+        });
+        return mapper;
     }
 }
+
+/*
+The ModelMapper instance is configured with a custom converter to handle PointDto to Point conversions.
+ When modelMapper.map(pointDto, Point.class) is called, the custom converter is triggered.
+ The converter takes the PointDto object and passes it to the GeometryUtil.createPoint method.
+ The GeometryUtil.createPoint method:
+     Extracts the coordinates from the PointDto.
+     Creates a Coordinate object with these coordinates.
+     Uses the GeometryFactory to create a Point object from the Coordinate.
+ The resulting Point object is returned by the converter and assigned to the point variable.
+ */
