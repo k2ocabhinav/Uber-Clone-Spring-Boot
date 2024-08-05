1b38ffaecb9c1b16493ba1b4e532ceb879018840
Abhinav Prasad <k2ocabhinav22@gmail.com>
2024-08-05 23:20:37 +0530

    chores: Bug fixes
    In RideDto, Point was being returned instead of PointDto. 😭😤

--- ubercloneapp/src/main/java/com/github/k2ocabhinav/ubercloneapp/dto/RideDto.java
+++ ubercloneapp/src/main/java/com/github/k2ocabhinav/ubercloneapp/dto/RideDto.java
@@ -2,15 +2,17 @@ package com.github.k2ocabhinav.ubercloneapp.dto;
 
 import com.github.k2ocabhinav.ubercloneapp.entities.enums.PaymentMethod;
 import com.github.k2ocabhinav.ubercloneapp.entities.enums.RideStatus;
+import lombok.Data;
 import org.locationtech.jts.geom.Point;
 
 import java.time.LocalDateTime;
 
+@Data
 public class RideDto {
 
     private Long id;
-    private Point pickupLocation;
-    private Point dropOffLocation;
+    private PointDto pickupLocation;
+    private PointDto dropOffLocation;
     private LocalDateTime createdTime;
     private LocalDateTime startedAt;
     private LocalDateTime endedAt;
