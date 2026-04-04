69cd4a8736e92885c1e2c552e063473f39823d66
Abhinav <abhinav@Abhinavs-MacBook-Pro.local>
2026-04-04 23:58:27 +0530

    refactor: add indexes and fetch optimization to Wallet entity

--- ubercloneapp/src/main/java/com/github/k2ocabhinav/ubercloneapp/entities/Wallet.java
+++ ubercloneapp/src/main/java/com/github/k2ocabhinav/ubercloneapp/entities/Wallet.java
@@ -11,16 +11,37 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
+@Table(indexes = {
+        @Index(name = "idx_wallet_user", columnList = "user_id")
+})
 public class Wallet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ToString.Exclude
     @OneToOne(fetch = FetchType.LAZY, optional = false)
     private User user;
 
     private Double balance = 0.0;
 
+    @ToString.Exclude
     @OneToMany(mappedBy = "wallet", fetch = FetchType.LAZY)
     private List<WalletTransaction> transactions;
+
+    @Version
+    private Long version;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Wallet that)) return false;
+        return id != null && id.equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
