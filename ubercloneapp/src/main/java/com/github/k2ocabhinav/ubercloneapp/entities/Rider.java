f1658f6bcf97623454073b7fc34c351d31cd210c
Abhinav <abhinav@Abhinavs-MacBook-Pro.local>
2026-04-04 23:22:06 +0530

    refactor: add indexes and fetch optimization to Rider entity

--- ubercloneapp/src/main/java/com/github/k2ocabhinav/ubercloneapp/entities/Rider.java
+++ ubercloneapp/src/main/java/com/github/k2ocabhinav/ubercloneapp/entities/Rider.java
@@ -10,14 +10,33 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(indexes = {
+        @Index(name = "idx_rider_rating", columnList = "rating")
+})
 public class Rider {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @ToString.Exclude
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
     private Double rating;
+
+    @Version
+    private Long version;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Rider that)) return false;
+        return id != null && id.equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 
 }
